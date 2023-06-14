5daa8f0b9fcc4c88b76cb06da83ce1c7a4bf197a
nn <86272232+loop-nop@users.noreply.github.com>
2023-06-14 09:47:43 +0000

    rain init

---
+++ NN_test/Mask.gd
@@ -0,0 +1,65 @@
+extends Node2D
+
+export var target_vp_path : NodePath
+export var wind_dir: Vector2 = Vector2.ZERO
+export var gravity: Vector2 = Vector2(0, -98)
+export var update_length: float = 0.33
+export var update_fps: float = 60
+export var update_on_every_frame: bool = false
+
+onready var sprite_count: int = update_length * update_fps
+
+onready var update_time = (1/update_fps)
+var time_past: float
+
+onready var vp: Viewport = get_viewport()
+var tg_vp: Viewport
+var tg_to_vp: Vector2
+
+var sprites: Array = []
+var sprite_index: int = 0
+
+func _resize():
+	tg_to_vp = vp.get_size() / tg_vp.get_size()
+	for s in sprites:
+		s.scale = tg_to_vp
+
+
+func _ready():
+	set_process(false)
+	set_process_input(false)
+	if target_vp_path:
+		get_tree().connect("screen_resized", self, "_resize")
+		tg_vp = get_node(target_vp_path)
+		if tg_vp:
+			tg_vp
+			spawn_sprites()
+			set_process(true)
+
+func _process(delta):
+	time_past += delta
+	if time_past > update_time or update_on_every_frame:
+		sprite_index = (sprite_index + 1) % sprite_count
+		var sprite: Sprite = sprites[sprite_index]
+		sprite.texture.create_from_image(tg_vp.get_texture().get_data())
+		sprite.set_position( Vector2.ZERO )
+		
+		for s in sprites:
+			s.set_position( s.position + (gravity + wind_dir)*time_past )
+
+		time_past = 0 
+
+func create_sprite() -> Sprite:
+	var _sprite
+	_sprite = Sprite.new()
+	# _sprite.offset = tg_vp.get_size()*0.5
+	_sprite.scale = tg_to_vp
+	_sprite.set_position( Vector2(9001,9001) )
+	_sprite.texture = ImageTexture.new()
+	add_child(_sprite)
+	return _sprite
+
+func spawn_sprites():
+	tg_to_vp = vp.get_size() / tg_vp.get_size()
+	for i in range(sprite_count):
+		sprites.append(create_sprite())
---
+++ NN_test/PhysicTest.gd
@@ -0,0 +1,6 @@
+extends Node2D
+onready var vp = get_viewport()
+onready var wvp = $"CanvasLayer2/ViewportControlWorld/World"
+
+func _process(delta):
+    pass
