2b46437484ed7adf36424fd95b7e4580ab3b4f5e
AltTabber <mikeice414@gmail.com>
2023-06-18 01:13:35 +0400

    add boss sprite and boss scene

---
+++ src/characters/Boss.gd
@@ -0,0 +1,37 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var eyes: Array = [];
+onready var eyesNode: Node2D = $eyes;
+onready var eyesTimer: Timer = $EyesTimer;
+onready var closedEyeTimer: Timer = $CloseEyeTimer;
+var rand=RandomNumberGenerator.new()
+
+var closedEye;
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	eyes.append_array(eyesNode.get_children());
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_EyesTimer_timeout():
+	var index = rand.randi_range(0, eyes.size()-1);
+	closedEye = eyes[index];
+	eyes[index].set_visible(false);
+	closedEyeTimer.start();
+	
+
+func _on_CloseEyeTimer_timeout():
+	if closedEye :
+		closedEye.set_visible(true);
+		closedEye = null;
