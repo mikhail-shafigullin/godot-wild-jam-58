9b6799a58a5a93cbedbd1769120e9f7eecae1779
Anton <voxel.art@yandex.com>
2023-06-18 19:47:16 +0300

    поменял стартовую сцену, добавил настройки звука

--- src/scenes/screens/UpgradeScreen.gd
+++ src/scenes/screens/UpgradeScreen.gd
@@ -2,5 +2,11 @@ extends Node2D
 
 const SceneMain = preload("res://src/scenes/OverWorld.tscn")
 
+func _ready():
+	$AudioStreamPlayer.play()
+	
 func _on_Start_Launching_pressed():
 	State.sceneManager.transition(SceneMain)
+
+func _on_GlobalVolume_value_changed(value):
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), value)
