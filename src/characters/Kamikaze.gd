91c5742dcfc0b190ac2d341d8e660ee81c0de78a
neimzi <neimzi.zxc@gmail.com>
2023-06-19 00:28:01 +0500

    bug fixes

--- src/characters/Kamikaze.gd
+++ src/characters/Kamikaze.gd
@@ -4,13 +4,18 @@ extends EnemyBase
 
 export var reward: float = 35
 
+var blast_scene = load("res://src/scenes/effects/Blast.tscn")
+
 var player_position: Vector2
 
 func _ready():
-	pass 
+	set_collision_mask_bit(1, true)
 
 func _physics_process(delta: float):
 	pass
+
+func on_taking_damage(damage: float):
+	.on_taking_damage(damage)
 
 func fight(delta: float):
 	player_position = player.global_position
@@ -20,7 +25,14 @@ func fight(delta: float):
 	pass
 
 func die():
-	print("%s is dead. Reward: %s"% [ name, reward])
 	applied_force = Vector2.ZERO
 	applied_torque = deg2rad(360 * randf() - 180) * 100 * mass
 	State.world.score += reward
+	.die()
+
+
+func _on_Kamikaze_body_entered(body):
+	var blast = blast_scene.instance()
+	blast.global_transform = global_transform
+	get_parent().add_child(blast)
+	queue_free()
--- src/ship/bullet/Bullet.gd
+++ src/ship/bullet/Bullet.gd
@@ -1,8 +1,6 @@
 class_name Bullet
 extends RigidBody2D
 
-
-export var damage: float = 20
 var cannon: Cannon
 
 var blast_scene = load("res://src/scenes/effects/Blast.tscn")
@@ -11,12 +9,11 @@ var blast_scene = load("res://src/scenes/effects/Blast.tscn")
 func _ready():
 	pass # Replace with function body.
 
-func on_taking_damage():
+func on_taking_damage(damage: float):
 	queue_free()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
 
 func _on_Bullet_body_entered(body):
 	var blast = blast_scene.instance()
@@ -24,13 +21,14 @@ func _on_Bullet_body_entered(body):
 	cannon.core.get_parent().call_deferred("add_child", blast)
 	queue_free()
 
-
 func _on_ActivationTimer_timeout():
 	# $CollisionShape2D.set_deferred("disabled", false)
 	if not is_connected("body_entered", self, "_on_Bullet_body_entered"):
 		connect('body_entered', self, '_on_Bullet_body_entered')
 
-
 func _on_Bullet_body_exited(body):
 	if body == cannon:
 		connect('body_entered', self, '_on_Bullet_body_entered')
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
